d94e9583a487694d3868fb0fb3d9d9dca728d41e
Varadjaninn <nvaradjanin@gmail.com>
2021-03-15 16:37:38 +0100

    modification

--- exerciseWeek4Loops.js
+++ exerciseWeek4Loops.js
@@ -48,7 +48,11 @@ console.log (program);
 
 var a = [[1, 2, 1, 24],[8, 11, 9, 4],[7, 0, 7, 27]];
 
-console.log (a)
+for (var i = 0; i < a.length; i++) {
+    for (var j = 0; j < a[i].length; j++) {
+        console.log (a[i][j])
+    }
+}
 
 
 
