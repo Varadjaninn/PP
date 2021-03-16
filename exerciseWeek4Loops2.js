a97a57a75fc689037ada58c0da48ab194d7d8702
Varadjaninn <nvaradjanin@gmail.com>
2021-03-16 16:18:47 +0100

    Updated exercise

--- exerciseWeek4Loops2.js
+++ exerciseWeek4Loops2.js
@@ -158,14 +158,11 @@ var a = [4, 6, 2, 8, 2, 2];
 result = [];
 
 for (var i = 0; i < a.length; i++ ) {
-    if (e < a[i] || e > a[i]) {
-        result[i] = a[i];
-    } else result[i] = 
+    if (e !== a[i]) {
+        result[result.length] = a[i];
+    } 
 }
 console.log (result)
-
-
-
 
 
 /*
@@ -174,3 +171,9 @@ the value of the position is greater than the array length, print the error mess
 Input: e = 78, p = 3, a = [2, -2, 33, 12, 5, 8]
 Output: [2, -2, 33, 78, 12, 5, 8]
 */
+
+var e = 78;
+var p = 3;
+var a = [2, -2, 33, 12, 5, 8];
+var result = [];
+
